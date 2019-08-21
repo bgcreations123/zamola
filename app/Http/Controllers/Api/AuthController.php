bc8897c6672404a3168289c88d14245367a29df9
Gabriel Jumah <bgcreations123@gmail.com>
2019-08-21 11:01:45 +0300

    Migrations and seeders created

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
 
     	$accessToken = $user->createToken('authToken')->accessToken;
 
-    	return response()->json(['user' => $user, 'accessToken' => $accessToken]);
+    	return response(['user' => $user, 'accessToken' => $accessToken]);
     }
 
     public function login(Request $request)
@@ -40,7 +40,7 @@ class AuthController extends Controller
 
     	$accessToken = $user->createToken('authToken')->accessToken;
 
-    	return response()->json(['user' => $user, 'accessToken' => $accessToken]);
+    	return response(['user' => $user, 'accessToken' => $accessToken]);
     }
 
 }
---
+++ database/migrations/2019_08_21_072929_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('status_id')->unsigned();
+            $table->string('tracer');
+            $table->integer('shipment_category_id')->unsigned();
+            $table->integer('payment_method_id')->unsigned();
+            $table->integer('quantity');
+            $table->integer('weight');
+            $table->text('description');
+            $table->integer('length');
+            $table->integer('width');
+            $table->integer('height');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
